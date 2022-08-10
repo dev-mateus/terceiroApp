a1d9307e227b2f2efa93d91cd3e56d51f79598b8
Mateus Pereira <mateus.pereira4@gmail.com>
2022-08-10 20:42:24 -0300

    implementação das funções matemáticas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  num1 : string
+  num2 : string
+  resultado : string
+
   constructor() {}
+
+  somar(){
+    var n1 = parseFloat(this.num1)
+    var n2 = parseFloat(this.num2)
+
+    var soma = n1 + n2
+    this.resultado = soma.toString()
+  }
+
+  subtrair(){
+    var n1 = parseFloat(this.num1)
+    var n2 = parseFloat(this.num2)
+
+    var sub = n1 - n2
+    this.resultado = sub.toString() 
+  }
+
+  multiplicar(){
+    var n1 = parseFloat(this.num1)
+    var n2 = parseFloat(this.num2)
+
+    var mult = n1 * n2
+    this.resultado = mult.toString()
+  }
+
+  dividir(){
+    var n1 = parseFloat(this.num1)
+    var n2 = parseFloat(this.num2)
+    if(n2 != 0){
+      var div = n1 / n2
+      this.resultado = div.toString()
+    }
+    else{
+      this.resultado = "Divisor igual a 0"
+    }
+    
+  }
+
+
 
 }
